ec8f43483135d65f94ffda1f80992d053b07246b
Coder-Mentality20 <57949642+Coder-Mentality20@users.noreply.github.com>
2021-06-21 11:42:31 -0700

    JunitTesting

---
+++ backend/src/test/java/me/zhulin/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,38 @@
+package me.zhulin.shopapi;
+
+import me.zhulin.shopapi.api.UserController;
+import me.zhulin.shopapi.entity.ProductInfo;
+import me.zhulin.shopapi.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopApiApplicationTests {
+    //@Autowired
+    //UserController userController;
+
+    @Test
+    public void testAddProduct() {
+      //  User user = new User();
+        //user.setEmail("Hamid123@gmaail.com");
+        //user.setPassword("hamid123");
+        //user.setName("HAMID");
+        //user.setPhone("0677832415");
+        //user.setAddress("Qu Worod Rue Smara N°37");
+        //user.setActive(true);
+        //user.setRole("ROLE_MANAGER");
+        //ResponseEntity<User> outcome = userController.save(user);
+        //assertThat(outcome,is(equalTo("success")));
+    }
+
+}
+
